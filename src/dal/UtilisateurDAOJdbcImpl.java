61ad4bc52b292e846f63e02e3a9d5bba6152df8a
BIGOT GEOFFRAY <geoffray.bigot2017@campus-eni.fr>
2018-10-10 11:32:34 +0200

    1132 10102018 G

--- src/dal/UtilisateurDAOJdbcImpl.java
+++ src/dal/UtilisateurDAOJdbcImpl.java
@@ -15,8 +15,7 @@ import util.DBConnection;
 public class UtilisateurDAOJdbcImpl {
 
 	private static final String selectUsers = "Select * from Utilisateur;";
-	private static final String selectConnection = "Select * from Utilisateur where email =? and password =?";
-	
+	private static final String selectConnection = "Select nom, prenom, eMail, password from Utilisateur where email =? and password =?;";
 
 	public static ArrayList<Utilisateur> selectAll() throws SQLException {
 		Utilisateur unUtilisateur = null;
@@ -25,45 +24,48 @@ public class UtilisateurDAOJdbcImpl {
 		Statement stmt = null;
 		ResultSet rs = null;
 		try {
-			cnx = DBConnection.seConnecter();	
+			cnx = DBConnection.seConnecter();
 			stmt = cnx.createStatement();
-			rs= stmt.executeQuery(selectUsers);
-			//traitement du rs
-			while(rs.next()) {
-				unUtilisateur= new Utilisateur(rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5));	
+			rs = stmt.executeQuery(selectUsers);
+			// traitement du rs
+			while (rs.next()) {
+				unUtilisateur = new Utilisateur(rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5));
 				utilisateurs.add(unUtilisateur);
 			}
-		}catch (SQLException e) {
-			throw new SQLException("probleme UtilisateurDAO methode lister"+e.getMessage());
+		} catch (SQLException e) {
+			throw new SQLException("probleme UtilisateurDAO methode lister" + e.getMessage());
 		} finally {
 			try {
 				stmt.close();
 				cnx.close();
 			} catch (SQLException e) {
-				throw new SQLException("probleme UtilisateurDAO fermeture connexion"+e.getMessage());
-			}	
+				throw new SQLException("probleme UtilisateurDAO fermeture connexion" + e.getMessage());
+			}
 		}
 		return utilisateurs;
 	}
-	
+
 	public static Utilisateur selectConnection(String eMail, String pass) throws SQLException {
 		Utilisateur user = null;
-		
+
 		Connection cnx = null;
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
 		try {
-			cnx = DBConnection.seConnecter();	
+			cnx = DBConnection.seConnecter();
 			stmt = cnx.prepareStatement(selectConnection);
 			stmt.setString(1, eMail);
 			stmt.setString(2, pass);
-			//traitement du rs
-				user = new Utilisateur(rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5), new Profil("Profil1", "Profil1"),new Promotion("CDI2018", "CDI2018"));	
-			} finally {
-				stmt.close();
-				cnx.close();
-			}	
+			rs = stmt.executeQuery();
+			while (rs.next()) {
+				user = new Utilisateur(rs.getString("nom"), rs.getString("prenom"), rs.getString("eMail"),
+						rs.getString("password"));
+			}
+		} finally {
+			stmt.close();
+			cnx.close();
+		}
 		return user;
 	}
-	
+
 }
