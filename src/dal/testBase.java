ef419ce39602c760155ae146036d5ff749b3a9f9
BIGOT GEOFFRAY <geoffray.bigot2017@campus-eni.fr>
2018-10-10 14:11:30 +0200

    1411 10102018 G

--- src/dal/testBase.java
+++ src/dal/testBase.java
@@ -3,15 +3,44 @@ package dal;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import bo.Profil;
+import bo.Promotion;
 import bo.Utilisateur;
 
-public class testDAO {
+public class testBase {
 
 	public static void main(String[] args) throws SQLException {
 		// TODO Auto-generated method stub
-		ArrayList<Utilisateur> select = UtilisateurDAOJdbcImpl.select();
-		Utilisateur utilisateur = select.get(0);
-		System.out.println(utilisateur.getPrenom()+  " "+utilisateur.getNom());
+		/*ArrayList<Promotion> selectPromo = PromotionDAOJdbcImpl.selectAll();
+		for(Promotion p : selectPromo) {
+			System.out.println(p.getCodePromotion()+  " "+p.getLibelle());			
+		}
+		
+		System.out.println("==============================");
+		ArrayList<Profil> selectProfil = ProfilDAOJdbcImpl.selectAll();
+		for(Profil p : selectProfil) {
+			System.out.println(p.getCodeProfil()+  " "+p.getLibelle());			
+		}
+		
+		System.out.println("==============================");
+		ArrayList<Utilisateur> selectUtilisateur = UtilisateurDAOJdbcImpl.selectAll();
+		for(Utilisateur p : selectUtilisateur) {
+			System.out.println(p.getNom()+ " "+p.getPrenom() +" "+p.getEmail() + " "
+					+p.getPassword()+" " +p.getProfil()+ " "+p.getPromotion());			
+		}
+		System.out.println("==============================");
+		Utilisateur userCo = UtilisateurDAOJdbcImpl.selectConnection("geoffray@bigot.com", "TestMdp");
+		System.out.println(userCo.getNom()+" "+userCo.getPrenom());
+			
+		/System.out.println("==============================");
+		userCo.setPrenom("GEOFFRAY");
+		UtilisateurDAOJdbcImpl.updateUtilisateur(userCo);
+		ArrayList<Utilisateur> selectUtilisateur = UtilisateurDAOJdbcImpl.selectAll();
+		for(Utilisateur p : selectUtilisateur) {
+			System.out.println(p.getNom()+ " "+p.getPrenom() +" "+p.getEmail() + " "
+					+p.getPassword()+" " +p.getProfil()+ " "+p.getPromotion());			
+		}*/
+		
+		
 	}
-
 }
