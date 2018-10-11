34c6bb0cf0e31c363c638cc2318dd92ab419264e
BIGOT GEOFFRAY <geoffray.bigot2017@campus-eni.fr>
2018-10-11 09:25:29 +0200

    0925 11102018 RelectureGeoffray

--- src/dal/SectionDAOJdbcImpl.java
+++ src/dal/SectionDAOJdbcImpl.java
@@ -12,24 +12,24 @@ import bo.Theme;
 import util.DBConnection;
 
 public class SectionDAOJdbcImpl {
-	private static final String selectSelonTest = "Select t.libelle,s.nbQuestionsATirer,s.idTest, s.idTheme  from Section_Test s"
+	private static final String selectSelonTest = "Select t.libelle,s.nbQuestionsATirer, s.idTheme  from Section_Test s"
 			+ " join Theme t on t.idTheme = s.idTheme"
 			+ " where s.idTest = ?;";
 	public static ArrayList<Section> selectSelonTest(int idTest) throws SQLException {
 		ArrayList<Section> sections = new ArrayList<>();
 		
 		Connection cnx = null;
-		Statement stmt = null;
+		PreparedStatement stmt = null;
 		ResultSet rs = null;
 	
 		try {
 			cnx = DBConnection.seConnecter();
-			PreparedStatement prep1 = cnx.prepareStatement(selectSelonTest); 
-			prep1.setInt(1, idTest);
-			rs= prep1.executeQuery();
+			stmt = cnx.prepareStatement(selectSelonTest); 
+			stmt.setInt(1, idTest);
+			rs= stmt.executeQuery();
 			//traitement du rs
 			while(rs.next()) {
-				sections.add(new Section(rs.getInt(2),new Theme(rs.getInt(4),rs.getString(1))));
+				sections.add(new Section(rs.getInt(2),new Theme(rs.getInt(3),rs.getString(1))));
 			}
 		}catch (SQLException e) {
 			throw new SQLException("probleme SectionDAO methode lister"+e.getMessage());
--- src/dal/ThemeDAOJdbcImpl.java
+++ src/dal/ThemeDAOJdbcImpl.java
@@ -5,12 +5,15 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 import bo.Theme;
 import util.DBConnection;
 
 public class ThemeDAOJdbcImpl {
 	private static final String selectById = "Select * from Theme t where t.idTheme = ?;";
+	private static final String selectAll = "Select * from Theme t";
+	
 	public static Theme selectbyId(int idTheme) throws SQLException {
 		Theme vTheme = null;
 		Connection cnx = null;
@@ -35,5 +38,30 @@ public class ThemeDAOJdbcImpl {
 			}
 		}
 		return vTheme;
+	}
+	
+	public static ArrayList<Theme> selectAll() throws SQLException {
+		ArrayList<Theme> themes = new ArrayList<>();
+		Connection cnx = null;
+		ResultSet rs = null;
+	
+		try {
+			cnx = DBConnection.seConnecter();
+			Statement prep1 = cnx.createStatement(); 
+			rs= prep1.executeQuery(selectAll);
+			//traitement du rs
+			while(rs.next()){
+			 themes.add(new Theme(rs.getInt(1), rs.getString(2)));
+			}
+		}catch (SQLException e) {
+			throw new SQLException("probleme ThemeDAO methode selectById "+e.getMessage());
+		} finally {
+			try {
+				cnx.close();
+			} catch (SQLException e) {
+				throw new SQLException("probleme ThemeDAO fermeture connexion"+e.getMessage());
+			}
+		}
+		return themes;
 	}
 }
