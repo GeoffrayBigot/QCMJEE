8f2dd135a701d048c1dc303d34a1e6b928b2990a
BIGOT GEOFFRAY <geoffray.bigot2017@campus-eni.fr>
2018-10-11 09:41:33 +0200

    0941 11102018 G

--- src/bll/GestionConnexion.java
+++ src/bll/GestionConnexion.java
@@ -9,14 +9,46 @@ public class GestionConnexion {
 
 	private static UtilisateurDAOJdbcImpl utilisateurDAO = new UtilisateurDAOJdbcImpl();
 	
-	public static Utilisateur connexion(String eMail, String pass) throws SQLException {
+	public static Utilisateur connexion(String eMail, String pass) throws Exception {
 		Utilisateur user = null ;
-		if(eMail != null && pass!=null) {
-			if(!eMail.isEmpty() && !pass.isEmpty()) {
+		
+		if(validationEmail(eMail)) {
+			if(validationMotsDePasse(pass)) {
 			 user = utilisateurDAO.selectConnection(eMail, pass);
+			}else {
+				throw new Exception("Votre Mot de Passe est incorrect !");
 			}
+		} else {
+			throw new Exception("Votre Email est incorrect !");
 		}
 		return user;			
 	}
 
+	/**
+	 * Valide l'adresse mail saisie.
+	 */
+	private static boolean validationEmail( String email ) throws Exception {
+	    if ( email != null && email.trim().length() != 0 ) {
+	        if ( !email.matches( "([^.@]+)(\\.[^.@]+)*@([^.@]+\\.)+([^.@]+)" ) ) {
+	           return false;
+	        } else {
+	        	return true;
+	        }
+	    } else {
+	        return false;
+	    }
+	}
+
+	/**
+	 * Valide les mots de passe saisis.
+	 */
+	private static boolean validationMotsDePasse( String motDePasse) throws Exception{
+	    if (motDePasse != null && motDePasse.trim().length() != 0 ) {
+	        	return true;
+	    } else {
+	    	return false;
+	    }
+	}
+
+	
 }
