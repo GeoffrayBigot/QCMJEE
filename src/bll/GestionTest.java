b16ea4a85f913bb84e6cf8c0bb03a77713e97ff8
GODICHEAUTORNIER RONAN <ronan.godicheautornier2017@campus-eni.fr>
2018-10-12 14:59:06 +0200

    1459 12102018 r

--- src/bll/GestionTest.java
+++ src/bll/GestionTest.java
@@ -1,5 +1,21 @@
 package bll;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import bo.Test;
+import dal.TestDAOJdbcImpl;
+
 public class GestionTest {
 
+	private static TestDAOJdbcImpl testsDAO = new TestDAOJdbcImpl();
+	
+	public ArrayList<Test> selectAllTest() throws SQLException {
+		try {
+			return testsDAO.selectAll();	
+
+		} catch (SQLException e) {
+			throw new SQLException("probleme GestionTheme fermeture connexion" + e.getMessage());
+		}
+	}
 }
